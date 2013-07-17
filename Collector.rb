f5844a258230952f5f2c755793dce76651915c1e
tolgap <tolga@hoppinger.com>
2013-07-17 12:20:02 +0200

    use Highline to style STDOUT messages

--- Collector.rb
+++ Collector.rb
@@ -1,109 +1,119 @@
-  require 'highline/import'
-  require 'json'
-  require 'net/http'
-  require 'uri'
-  require 'php_serialize'
+require 'highline/import'
+require 'json'
+require 'net/http'
+require 'uri'
+require 'php_serialize'
 
-  require './config/settings'
+require './config/settings'
 
-  class Collector
+class Collector
 
-    @config = Settings.config
+  @config = Settings.config
+  @@wp_current_ver = ""
 
-    @@dir    = @config[:vhost_folders]
-    @@host   = @config[:master_server]
-    @@port   = @config[:master_server_port]
-    @@server = @config[:client_name]
+  @@dir    = @config[:vhost_folders]
+  @@host   = @config[:master_server]
+  @@port   = @config[:master_server_port]
+  @@server = @config[:client_name]
 
-    unless @config[:htpasswd_user].empty?
-      @@user = @config[:htpasswd_user] 
-    else
-      @@user = nil
-    end
-
-    unless @config[:htpasswd_pass].empty?
-      @@pass = @config[:htpasswd_pass] 
-    else
-      @@pass = nil
-    end
-
-    def collect_all
-      all = Array.new
-      Dir.glob("#{@@dir}**/wp-config.php").each do |wp_dir|
-        wp = wp_dir[0..-15].sub!(@@dir, "")
-        data = gather(wp, true)
-
-        if (data != false)
-          all.push(data)
-        else
-          say("<%= color('Error:', :red) %> #{wp} project has either database issues, or other PHP issues. Check your installation")
-        end
-      end
-
-      complete = {"server" => @@server, "data" => all}
-
-      path_all = "/save-all"
-      send_result(path_all, complete)
-    end
-
-    def gather (website_folder, extract)
-      if (extract)
-        data          = @@dir + website_folder
-        result        = `cd #{data} && wp plugin list --format=json`
-        name          = `cd #{data} && wp option get blogname`
-        versionresult = `cd #{data} && wp core version`
-      end
-
-      begin
-        plugins       = JSON.parse(sanitize(result, "plugin"))
-        blog_name     = sanitize(name, "blogname")
-        version       = sanitize(versionresult, "version")
-
-        array  = { "name" => website_folder, "blog_name" => blog_name,"version" => version, "plugins" => plugins }
-      rescue
-        array = false
-      end
-
-      array
-    end
-
-    def sanitize(result, type="plugin")
-      result = result.split("\n")
-      if (type === "plugin")
-        plugins = result.last
-        return plugins
-      elsif (type === "blogname")
-        blog_name = result.last
-        return blog_name
-      else
-        versionresult = result.last
-        return versionresult
-      end
-    end
-
-    def send_result (path, result)
-      request      = Net::HTTP::Post.new(path, initheader = {'Content-Type' => 'application/json'})
-      unless (@@user.nil? && @@pass.nil?)
-        request.basic_auth @@user, @@pass
-      end
-      request.body = result.to_json
-      response     = Net::HTTP.new(@@host, @@port).start { |http| http.request(request) }
-    end
-
-    def check_latest_wp_version
-      url = URI.parse('http://api.wordpress.org/core/version-check/1.6')
-      r = Net::HTTP.get_response(url)
-      if r.code == "301"
-          url = URI.parse(r.header['location'])
-      end
-
-      req = Net::HTTP::Get.new(url.path)
-      res = Net::HTTP.start(url.host, url.port) {|http|
-        http.request(req)
-      }
-
-      result = PHP.unserialize(res.body)
-      result['offers'].first['current']
-    end
-
+  unless @config[:htpasswd_user].empty?
+    @@user = @config[:htpasswd_user] 
+  else
+    @@user = nil
   end
+
+  unless @config[:htpasswd_pass].empty?
+    @@pass = @config[:htpasswd_pass] 
+  else
+    @@pass = nil
+  end
+
+  def collect_all
+    @wp_current_ver = Collector.check_latest_wp_version
+
+    all = Array.new
+    Dir.glob("#{@@dir}**/wp-config.php").each do |wp_dir|
+      wp = wp_dir[0..-15].sub!(@@dir, "")
+      data = gather(wp, true)
+
+      if (data != false)
+        all.push(data)
+      else
+        say("<%= color('Error:', :red) %> #{wp} project has either database issues, or other PHP issues. Check your installation")
+      end
+    end
+
+    complete = {"server" => @@server, "data" => all}
+
+    path_all = "/save-all"
+    send_result(path_all, complete)
+  end
+
+  def gather (website_folder, extract)
+    if (extract)
+      data          = @@dir + website_folder
+      result        = `cd #{data} && wp plugin list --format=json`
+      name          = `cd #{data} && wp option get blogname`
+      versionresult = `cd #{data} && wp core version`
+    end
+
+    begin
+      plugins       = JSON.parse(sanitize(result, "plugin"))
+      blog_name     = sanitize(name, "blogname")
+      version       = sanitize(versionresult, "version")
+      has_update    = version === @wp_current_ver ? "none" : "available"
+
+      array  = {
+        "name"       => website_folder,
+        "blog_name"  => blog_name,
+        "has_update" => has_update,
+        "version"    => version,
+        "plugins"    => plugins
+      }
+    rescue
+      array = false
+    end
+
+    array
+  end
+
+  def sanitize(result, type="plugin")
+    result = result.split("\n")
+    if (type === "plugin")
+      plugins = result.last
+      return plugins
+    elsif (type === "blogname")
+      blog_name = result.last
+      return blog_name
+    else
+      versionresult = result.last
+      return versionresult
+    end
+  end
+
+  def send_result (path, result)
+    request      = Net::HTTP::Post.new(path, initheader = {'Content-Type' => 'application/json'})
+    unless (@@user.nil? && @@pass.nil?)
+      request.basic_auth @@user, @@pass
+    end
+    request.body = result.to_json
+    response     = Net::HTTP.new(@@host, @@port).start { |http| http.request(request) }
+  end
+
+  def self.check_latest_wp_version
+    url = URI.parse('http://api.wordpress.org/core/version-check/1.6')
+    r = Net::HTTP.get_response(url)
+    if r.code == "301"
+        url = URI.parse(r.header['location'])
+    end
+
+    req = Net::HTTP::Get.new(url.path)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+
+    result = PHP.unserialize(res.body)
+    result['offers'].first['current']
+  end
+
+end
--- config/example-settings.rb
+++ config/example-settings.rb
@@ -1,12 +1,11 @@
 module Settings
   @settings = {
     :client_name        => "my local projects", # A unique name for this Collector client.
-    :master_server      => "localhost",         # For example 'http://www.server.com/path'.
-    :master_server_port => "4567",              # The port for your server. Default Sinatra port set.
+    :master_server      => "localhost",         # For example 'server.com/path'. Don't add www or http://
+    :master_server_port => "3000",              # The port for your server. Default Rails port set.
     :interval_unit      => "minute",            # For example "days", "week", "hour".
     :interval_quantity  => 10,                  # How often you want to run the info job based on your interval unit.
     :vhost_folders      => "/path/to/vhosts/",  # This point to your directory where all your project folders are located.
-    :wp_root_folder     => "httpdocs",          # The folder name of all your wp installs. It usually is httpdocs.
     :htpasswd_user      => "",                  # The basic auth user.
     :htpasswd_pass      => ""                   # The basic auth password.
   }
