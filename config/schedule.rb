2ec4b2a866257a08c72fde7c42c0abb19be4e601
tolgap <tolga@hoppinger.com>
2014-04-16 22:27:15 +0200

    added support for cms specific collecting

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,9 +3,12 @@ env :PATH, ENV['PATH']
 require './config/settings'
 require './lib/collector'
 
+cms      = Settings.config[:cms]
 interval = Settings.config[:interval_quantity]
 unit     = Settings.config[:interval_unit]
 
 every interval.send(unit) do
-  rake "collect_all"
+  cms.each do |cms_type|
+    Thread.new { rake "#{cms_type}:collect_all" }
+  end
 end
