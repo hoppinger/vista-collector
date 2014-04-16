60f1b1f421b4222b9df0c9ee83c3702195fa50b0
tolgap <tolga@hoppinger.com>
2014-04-16 22:52:19 +0200

    forgot a comma

--- config/example-settings.rb
+++ config/example-settings.rb
@@ -1,14 +1,14 @@
 module Settings
   @settings = {
-    :client_name        => "my local projects",  # A unique name for this Collector client.
-    :master_server      => "localhost",          # For example 'server.com/path'. Don't add www or http://
-    :master_server_port => "3000",               # The port for your server. Default Rails port set.
-    :cms                => [:wordpress, :drupal] # Which CMSs do you want to index
-    :interval_unit      => "minute",             # For example "days", "week", "hour".
-    :interval_quantity  => 10,                   # How often you want to run the info job based on your interval unit.
-    :vhost_folders      => "/path/to/vhosts/",   # This point to your directory where all your project folders are located.
-    :htpasswd_user      => nil,                  # The basic auth user.
-    :htpasswd_pass      => nil                   # The basic auth password.
+    :client_name        => "my local projects",   # A unique name for this Collector client.
+    :master_server      => "localhost",           # For example 'server.com/path'. Don't add www or http://
+    :master_server_port => "3000",                # The port for your server. Default Rails port set.
+    :cms                => [:wordpress, :drupal], # Which CMSs do you want to index
+    :interval_unit      => "minute",              # For example "days", "week", "hour".
+    :interval_quantity  => 10,                    # How often you want to run the info job based on your interval unit.
+    :vhost_folders      => "/path/to/vhosts/",    # This point to your directory where all your project folders are located.
+    :htpasswd_user      => nil,                   # The basic auth user.
+    :htpasswd_pass      => nil                    # The basic auth password.
   }
 
   def self.config
