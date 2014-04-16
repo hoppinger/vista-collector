1fdeb244462ab5420ed4d734dfd807cd6a2cbc3a
tolgap <tolga@hoppinger.com>
2014-04-16 22:28:36 +0200

    moved wordpress specific classes to it's own module

---
+++ lib/collector/wordpress/client.rb
@@ -0,0 +1,39 @@
+module Collector
+  module Wordpress
+
+    class Client < Collector::Client
+
+      def initialize
+        super
+        @version = check_latest_wp_version
+      end
+
+      def collect_all
+        installs = find_wordpress_installs
+
+        installs.each do |wp_install|
+          website = Collector::Website.new(@config[:vhost_folders], wp_install)
+          add_website(website)
+          result = self.collect_single(website)
+        end
+      end
+
+      def find_wordpress_installs
+        wp_directories = find_installs ["wp-config.php"]
+      end
+
+      def collect_single(website)
+        command = Collector::Wordpress::Command.new(website)
+        website.type = :wordpress
+        begin
+          command.blog_name; command.version; command.plugins
+          true
+        rescue
+          false
+        end
+      end
+
+    end
+
+  end
+end
