ca124ee86d9c35d1cdbc731aab45507688d16ab2
MarkArts <markarts@hoppinger.com>
2017-04-06 13:15:58 +0200

    Add symfony matching for newer symfony projects

--- lib/collector/symfony/command.rb
+++ lib/collector/symfony/command.rb
@@ -7,37 +7,64 @@ module Collector
     class Command < Collector::Command
 
       attr_accessor :current
-      attr_accessor :ini
-
       def initialize(current)
         super
         @current = current
-        @ini = IniFile.load(@current.dir + '/deps')
+        @ini_file = @current.dir + '/deps'
+        @composer_file = Pathname.new(@current.dir).parent.join('composer.lock')
       end
 
       def blog_name
-
+        
       end
 
       def version
-        @current.version = @ini['symfony']['version'].gsub('v', '')
+        plugins = _plugins
+        if plugins.any?
+          @current.version = plugins.select { |p| p[:name] == 'symfony' }.first[:version].sub('v', '')
+        else
+          @current.version = ''
+        end
       end
 
       def plugins
-        plugins = []
-        @ini.each_section do |section|
-          plugins << {
+        @current.plugins = _plugins
+      end
+
+      def _plugins
+        if File.exist?(@ini_file)
+          ini_plugins(@ini_file)
+        elsif File.exist?(@composer_file)
+          composer_plugins(@composer_file)
+        else
+          []
+        end
+      end
+
+      def ini_plugins(ini_file)
+        @ini = IniFile.load(@current.dir + '/deps')
+
+        @ini.sections.map do |section|
+          {
             name: section,
             status: :active,
             update: :none,
             version: @ini[section]['version']
           }
         end
-
-        @current.plugins = plugins
       end
 
+      def composer_plugins(composer_file)
+        composer = JSON.parse(File.read(composer_file))
+        composer['packages'].map do |p|
+          {
+            name: p['name'].split('/')[1],
+            status: :active,
+            update: :none,
+            version: p['version']
+          }
+        end
+      end
     end
-
   end
 end
