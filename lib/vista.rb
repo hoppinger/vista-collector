4c1f1d1fc15787114a53bd55421facb37cd7ba09
Marcel Dillen <marceldillen@me.com>
2017-08-23 14:13:24 +0200

    Make vista request optional

--- lib/vista.rb
+++ lib/vista.rb
@@ -54,22 +54,24 @@ class Vista
 
       puts request.send('/collector', server)
 
-      request2 = RequestVista.new(
-        @config[:vista_server],
-        @config[:master_server_port],
-        {
-          user: @config[:htpasswd_user],
-          pass: @config[:htpasswd_pass]
+      if @config.key?("vista_server")
+        request2 = RequestVista.new(
+          @config[:vista_server],
+          @config[:master_server_port],
+          {
+            user: @config[:htpasswd_user],
+            pass: @config[:htpasswd_pass]
+          }
+        )
+
+        # convert the array of object to a hash
+        server2 = {
+          websites: @websites.map{ |w| w.to_hash(@version).merge({server: @config[:client_name].underscore }) }.map{ |w| w[:website] },
+          name: @config[:client_name].underscore
         }
-      )
 
-      # convert the array of object to a hash
-      server2 = {
-        websites: @websites.map{ |w| w.to_hash(@version).merge({server: @config[:client_name].underscore }) }.map{ |w| w[:website] },
-        name: @config[:client_name].underscore
-      }
-
-      request.send('/servers', server)
+        request.send('/servers', server)
+      end
   end
 
 end
